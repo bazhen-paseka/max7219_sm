d35e269e170a416abb2b972ef791916b6b35fb17
bazhen-win7 <bazhen.paseka@gmail.com>
2022-05-03 17:17:51 +0300

    2x8 it work

--- max7219_digit.c
+++ max7219_digit.c
@@ -1,115 +1,64 @@
-
-//#include "stm32f1xx_hal.h"
 #include "max7219_digit.h"
 
-#define BYTE_IN_SPI_PACKAGE		2
-#define SPI_PACKAGE_TIMEOUT		2
 
-void _max7219_print_one_digit(max7219_struct max7219_handler, uint8_t position, uint32_t value);
-//void _max7219_write_strob(max7219_struct max7219_handler);
+void _max7219_print_one_digit	( max7219_struct 	_max7219_handler 	,
+								uint8_t 			_position 			,
+								uint32_t 			_value1				,
+								uint32_t 			_value2				) ;
 
-//void _max7219_print_one_digit	( max7219_struct 	_max7219_handler 	,
-//								uint8_t 			_position 			,
-//								uint32_t 			_value 				) ;
-
-//void _max7219_show_all			( max7219_struct 	_max7219_handler	) ;
 void _max7219_write_strob		( max7219_struct 	_max7219_handler	) ;
 void _max7219_push_data			( max7219_struct 	_max7219_handler	) ;
-//uint8_t inverse_order_in_byte	( uint8_t 			_input 				) ;
-void Local_delay				( uint32_t 			_delay_u32			) ;
+void _local_delay				( uint32_t 			_delay_u32			) ;
+uint8_t inverse_order_in_byte	( uint8_t 			_input 				) ;
 
 /***************************************************************************************/
 
-
-void max7219_init(max7219_struct *max7219_handler)
-{
-	uint8_t spi_buffer[2];
-
-	HAL_GPIO_WritePin(max7219_handler->cs_port, max7219_handler->cs_pin, RESET);
-	//_max7219_write_strob(*max7219_handler);
-
-	// test - On
-//	spi_buffer[0] = 0x0F;  spi_buffer[1] = 0x01;
-//	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-//	_max7219_write_strob(*max7219_handler);
-
-	HAL_Delay(100);
-
-	// test - Off
-	spi_buffer[0] = 0x0F;  spi_buffer[1] = 0x00;
-	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(*max7219_handler);
-
-	// Decode Mode - No 1 in 1
-	// spi_buffer[1] hex   -> FF
-	// spi_buffer[1] pixel -> 00
-	uint8_t DecodeMode = 0xFF;
-	spi_buffer[0] = 0x09;  spi_buffer[1] = DecodeMode;	// pixel
-	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(*max7219_handler);
-
-	// Intensity 3/32 0x01,
-	spi_buffer[0] = 0x0A;  spi_buffer[1] = 0x03;
-	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(*max7219_handler);
-
-	//Scan Limit - All
-	spi_buffer[0] = 0x0B;  spi_buffer[1] = 0x07;
-	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(*max7219_handler);
-
-	// Shutdown - none
-	// spi_buffer[1] -> 00 sleep
-	// spi_buffer[1] -> 01 work
-	spi_buffer[0] = 0x0C;  spi_buffer[1] = 0x01;
-	HAL_SPI_Transmit(max7219_handler->spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(*max7219_handler);
-}
-/***************************************************************************************/
-
-void max7219_print_value(max7219_struct *max7219_handler,uint32_t value, position_enum position)
-{
-	if (position == 0)
-	{
-		position = 4;
-	}
-	else
-	{
-		position = 0;
-	}
-	_max7219_print_one_digit(*max7219_handler, position + 8, (value/10000000) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 7, (value/1000000 ) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 6, (value/100000  ) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 5, (value/10000   ) % 10 );
-
-	_max7219_print_one_digit(*max7219_handler, position + 4, (value/1000) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 3, (value/100 ) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 2, (value/10  ) % 10 );
-	_max7219_print_one_digit(*max7219_handler, position + 1, (value     ) % 10 );
-}
-/***************************************************************************************/
-/***************************************************************************************/
-
-void _max7219_print_one_digit(max7219_struct max7219_handler, uint8_t position, uint32_t value)
-{
-	uint8_t spi_buffer[2];
-	spi_buffer[0] = position	; // razryad
-	spi_buffer[1] = value	; // znachenie
-	HAL_SPI_Transmit(max7219_handler.spi, spi_buffer, BYTE_IN_SPI_PACKAGE, SPI_PACKAGE_TIMEOUT);
-	_max7219_write_strob(max7219_handler);
-}
-/***************************************************************************************/
-
-//void _max7219_write_strob(max7219_struct max7219_handler)
-//{
-//	HAL_Delay(1);
-//	HAL_GPIO_WritePin(max7219_handler.cs_port, max7219_handler.cs_pin, SET);
-//	HAL_Delay(1);
-//	HAL_GPIO_WritePin(max7219_handler.cs_port, max7219_handler.cs_pin, RESET);
-//	//HAL_Delay(1);
+//void max7219_init2(	max7219_struct 			*_max7219_handler	,
+//					max7219_Decode_Mode 	_decodemode			,
+//					max7219_LED_Intensity	_intensity			,
+//					max7219_Scan_Limit 		_scanlimit			,
+//					max7219_Shutdown 		_shutdown 			) {
+//
+//	// test - Off
+//	_max7219_handler->data[0] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[1] = WorkMode ;
+//	_max7219_handler->data[2] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[3] = WorkMode ;
+//	_max7219_handler->data[4] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[5] = WorkMode ;
+//	_max7219_handler->data[6] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[7] = WorkMode ;
+//	_max7219_push_data( *_max7219_handler ) ;
+//
+//	// Decode Mode - No. 1 in 1
+//	_max7219_handler->data[0] = ADDR_DECODE_MODE ;  _max7219_handler->data[1] = _decodemode ;
+//	_max7219_handler->data[2] = ADDR_DECODE_MODE ;  _max7219_handler->data[3] = _decodemode ;
+//	_max7219_handler->data[4] = ADDR_DECODE_MODE ;  _max7219_handler->data[5] = _decodemode ;
+//	_max7219_handler->data[6] = ADDR_DECODE_MODE ;  _max7219_handler->data[7] = _decodemode ;
+//	_max7219_push_data( *_max7219_handler ) ;
+//
+//	// Intensity x/32
+//	_max7219_handler->data[0] = ADDR_INTENSITY ;  _max7219_handler->data[1] = _intensity ;
+//	_max7219_handler->data[2] = ADDR_INTENSITY ;  _max7219_handler->data[3] = _intensity ;
+//	_max7219_handler->data[4] = ADDR_INTENSITY ;  _max7219_handler->data[5] = _intensity ;
+//	_max7219_handler->data[6] = ADDR_INTENSITY ;  _max7219_handler->data[7] = _intensity ;
+//	_max7219_push_data( *_max7219_handler ) ;
+//
+//	//Scan Limit - All
+//	_max7219_handler->data[0] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[1] = _scanlimit ;
+//	_max7219_handler->data[2] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[3] = _scanlimit ;
+//	_max7219_handler->data[4] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[5] = _scanlimit ;
+//	_max7219_handler->data[6] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[7] = _scanlimit ;
+//	_max7219_push_data( *_max7219_handler ) ;
+//
+//	// Shutdown - none
+//	// myTrans[1] -> 00 sleep
+//	// myTrans[1] -> 01 work
+//	_max7219_handler->data[0] = ADDR_SHUTDOWN;  _max7219_handler->data[1] = _shutdown;
+//	_max7219_handler->data[2] = ADDR_SHUTDOWN;  _max7219_handler->data[3] = _shutdown;
+//	_max7219_handler->data[4] = ADDR_SHUTDOWN;  _max7219_handler->data[5] = _shutdown;
+//	_max7219_handler->data[6] = ADDR_SHUTDOWN;  _max7219_handler->data[7] = _shutdown;
+//	_max7219_push_data( *_max7219_handler ) ;
 //}
 /***************************************************************************************/
 
+/***************************************************************************************/
 
 void max7219_init2(	max7219_struct 			*_max7219_handler	,
 					max7219_Decode_Mode 	_decodemode			,
@@ -118,65 +67,103 @@ void max7219_init2(	max7219_struct 			*_max7219_handler	,
 					max7219_Shutdown 		_shutdown 			) {
 
 	// test - Off
-	_max7219_handler->data[0] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[1] = WorkMode ;
-	_max7219_handler->data[2] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[3] = WorkMode ;
-	_max7219_handler->data[4] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[5] = WorkMode ;
-	_max7219_handler->data[6] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[7] = WorkMode ;
+	_max7219_handler->data[ 0] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[ 1] = WorkMode ;
+	_max7219_handler->data[ 2] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[ 3] = WorkMode ;
+	_max7219_handler->data[ 4] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[ 5] = WorkMode ;
+	_max7219_handler->data[ 6] = ADDR_DISPLAY_TEST ;  _max7219_handler->data[ 7] = WorkMode ;
 	_max7219_push_data( *_max7219_handler ) ;
 
 	// Decode Mode - No. 1 in 1
-	_max7219_handler->data[0] = ADDR_DECODE_MODE ;  _max7219_handler->data[1] = _decodemode ;
-	_max7219_handler->data[2] = ADDR_DECODE_MODE ;  _max7219_handler->data[3] = _decodemode ;
-	_max7219_handler->data[4] = ADDR_DECODE_MODE ;  _max7219_handler->data[5] = _decodemode ;
-	_max7219_handler->data[6] = ADDR_DECODE_MODE ;  _max7219_handler->data[7] = _decodemode ;
+	_max7219_handler->data[ 0] = ADDR_DECODE_MODE ;  _max7219_handler->data[ 1] = _decodemode ;
+	_max7219_handler->data[ 2] = ADDR_DECODE_MODE ;  _max7219_handler->data[ 3] = _decodemode ;
+	_max7219_handler->data[ 4] = ADDR_DECODE_MODE ;  _max7219_handler->data[ 5] = _decodemode ;
+	_max7219_handler->data[ 6] = ADDR_DECODE_MODE ;  _max7219_handler->data[ 7] = _decodemode ;
 	_max7219_push_data( *_max7219_handler ) ;
 
 	// Intensity x/32
-	_max7219_handler->data[0] = ADDR_INTENSITY ;  _max7219_handler->data[1] = _intensity ;
-	_max7219_handler->data[2] = ADDR_INTENSITY ;  _max7219_handler->data[3] = _intensity ;
-	_max7219_handler->data[4] = ADDR_INTENSITY ;  _max7219_handler->data[5] = _intensity ;
-	_max7219_handler->data[6] = ADDR_INTENSITY ;  _max7219_handler->data[7] = _intensity ;
+	_max7219_handler->data[ 0] = ADDR_INTENSITY ;  _max7219_handler->data[ 1] = _intensity ;
+	_max7219_handler->data[ 2] = ADDR_INTENSITY ;  _max7219_handler->data[ 3] = _intensity ;
+	_max7219_handler->data[ 4] = ADDR_INTENSITY ;  _max7219_handler->data[ 5] = _intensity ;
+	_max7219_handler->data[ 6] = ADDR_INTENSITY ;  _max7219_handler->data[ 7] = _intensity ;
 	_max7219_push_data( *_max7219_handler ) ;
 
 	//Scan Limit - All
-	_max7219_handler->data[0] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[1] = _scanlimit ;
-	_max7219_handler->data[2] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[3] = _scanlimit ;
-	_max7219_handler->data[4] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[5] = _scanlimit ;
-	_max7219_handler->data[6] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[7] = _scanlimit ;
+	_max7219_handler->data[ 0] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[ 1] = _scanlimit ;
+	_max7219_handler->data[ 2] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[ 3] = _scanlimit ;
+	_max7219_handler->data[ 4] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[ 5] = _scanlimit ;
+	_max7219_handler->data[ 6] = ADDR_SCAN_LIMIT ;  _max7219_handler->data[ 7] = _scanlimit ;
 	_max7219_push_data( *_max7219_handler ) ;
 
 	// Shutdown - none
 	// myTrans[1] -> 00 sleep
 	// myTrans[1] -> 01 work
-	_max7219_handler->data[0] = ADDR_SHUTDOWN;  _max7219_handler->data[1] = _shutdown;
-	_max7219_handler->data[2] = ADDR_SHUTDOWN;  _max7219_handler->data[3] = _shutdown;
-	_max7219_handler->data[4] = ADDR_SHUTDOWN;  _max7219_handler->data[5] = _shutdown;
-	_max7219_handler->data[6] = ADDR_SHUTDOWN;  _max7219_handler->data[7] = _shutdown;
+	_max7219_handler->data[ 0] = ADDR_SHUTDOWN;  _max7219_handler->data[ 1] = _shutdown;
+	_max7219_handler->data[ 2] = ADDR_SHUTDOWN;  _max7219_handler->data[ 3] = _shutdown;
+	_max7219_handler->data[ 4] = ADDR_SHUTDOWN;  _max7219_handler->data[ 5] = _shutdown;
+	_max7219_handler->data[ 6] = ADDR_SHUTDOWN;  _max7219_handler->data[ 7] = _shutdown;
 	_max7219_push_data( *_max7219_handler ) ;
 }
 /***************************************************************************************/
-/***************************************************************************************/
 
 void _max7219_push_data( max7219_struct 	_max7219_handler ) {
-
     HAL_SPI_Transmit( _max7219_handler.spi , _max7219_handler.data , BYTE_IN_SPI_PACKAGE , SPI_PACKAGE_TIMEOUT ) ;
     _max7219_write_strob( _max7219_handler ) ;
 }
 /***************************************************************************************/
 
 void _max7219_write_strob( max7219_struct 	_max7219_handler ) {
-
 	HAL_GPIO_WritePin( _max7219_handler.cs_port , _max7219_handler.cs_pin , SET ) ;
-	//HAL_Delay( 1 ) ;
-	Local_delay( WRITE_STROB_DELAY ) ;
+	_local_delay( WRITE_STROB_DELAY ) ;
 	HAL_GPIO_WritePin( _max7219_handler.cs_port , _max7219_handler.cs_pin , RESET ) ;
 }
 /***************************************************************************************/
 
-void Local_delay ( uint32_t _delay_u32 ) {
-
+void _local_delay ( uint32_t _delay_u32 ) {
 	for ( ; _delay_u32 > 0; _delay_u32-- ) {
 		__asm( "nop" ) ;
 	}
+}
+/***************************************************************************************/
+
+void max7219_print_value(	max7219_struct	*max7219_handler	,
+							uint32_t 		value1				,
+							uint32_t		value2				,
+							position_enum 	position			) {
+
+	_max7219_print_one_digit(*max7219_handler, position + 4, (value1/1000) % 10 , (value2/1000) % 10 );
+	_max7219_print_one_digit(*max7219_handler, position + 3, (value1/100 ) % 10 , (value2/100 ) % 10 );
+	_max7219_print_one_digit(*max7219_handler, position + 2, (value1/10  ) % 10 , (value2/10  ) % 10 );
+	_max7219_print_one_digit(*max7219_handler, position + 1, (value1     ) % 10 , (value2     ) % 10 );
+}
+/***************************************************************************************/
+/***************************************************************************************/
+
+
+void _max7219_print_one_digit(	max7219_struct 	_max7219_handler	,
+								uint8_t 		_position			,
+								uint32_t 		_value1				,
+								uint32_t 		_value2				) {
+
+	uint8_t spi_buffer[4]		;
+	spi_buffer[0] = _position	; // razryad
+	spi_buffer[1] = _value1		; // znachenie
+	spi_buffer[2] = _position	; // razryad
+	spi_buffer[3] = _value2		; // znachenie
+	HAL_SPI_Transmit( _max7219_handler.spi , spi_buffer , 4 , SPI_PACKAGE_TIMEOUT ) ;
+	_max7219_write_strob( _max7219_handler ) ;
+}
+/***************************************************************************************/
+
+uint8_t inverse_order_in_byte (uint8_t 		input) {
+
+    uint8_t var_u8 =((input & 0x01) << 7) |
+					((input & 0x02) << 5) |
+					((input & 0x04) << 3) |
+					((input & 0x08) << 1) |
+					((input & 0x10) >> 1) |
+					((input & 0x20) >> 3) |
+					((input & 0x40) >> 5) |
+					((input & 0x80) >> 7) ;
+    return var_u8 ;
 }
 /***************************************************************************************/
--- max7219_digit.h
+++ max7219_digit.h
@@ -31,16 +31,18 @@
 **************************************************************************
 */
 
-typedef enum{
-	DisplayDigit_0_only	=	0x00 ,
-	DisplayDigit_0_1	=	0x01 ,
-	DisplayDigit_0_2	=	0x02 ,
-	DisplayDigit_0_3	=	0x03 ,
-	DisplayDigit_0_4	=	0x04 ,
-	DisplayDigit_0_5	=	0x05 ,
-	DisplayDigit_0_6	=	0x06 ,
-	DisplayDigit_0_7	=	0x07
-}		max7219_Scan_Limit ;
+typedef enum {
+	NoDecode			= 0x00 ,
+	DecodeForDigit_0	= 0x01 ,
+	DecodeForDigit_1_0	= 0x03 ,
+	DecodeForDigit_2_0	= 0x07 ,
+	DecodeForDigit_3_0	= 0x0F ,
+	DecodeForDigit_4_0	= 0x1F ,
+	DecodeForDigit_5_0	= 0x3F ,
+	DecodeForDigit_6_0	= 0x7F ,
+	DecodeForDigit_7_0	= 0xFF ,
+}		max7219_Decode_Mode ;
+//----------------------------------
 
 typedef enum {
 	Intensity_1		= 0x00 ,
@@ -62,32 +64,30 @@ typedef enum {
 }		max7219_LED_Intensity ;
 //----------------------------------
 
-typedef enum
-{
+typedef enum {
+	DisplayDigit_0_only	=	0x00 ,
+	DisplayDigit_0_1	=	0x01 ,
+	DisplayDigit_0_2	=	0x02 ,
+	DisplayDigit_0_3	=	0x03 ,
+	DisplayDigit_0_4	=	0x04 ,
+	DisplayDigit_0_5	=	0x05 ,
+	DisplayDigit_0_6	=	0x06 ,
+	DisplayDigit_0_7	=	0x07
+}		max7219_Scan_Limit ;
+//----------------------------------
+
+
+typedef enum {
 	ShutdownMode	= 0 ,
 	NormalOperation	= 1
 }		max7219_Shutdown ;
 //----------------------------------
 
-typedef enum
-{
+typedef enum {
 	WorkMode	= 0,
 	TestMode	= 1
 }		max7219_Display_Test ;
 //----------------------------------
-
-typedef enum
-{
-	NoDecode			= 0x00 ,
-	DecodeForDigit_0	= 0x01 ,
-	DecodeForDigit_1_0	= 0x03 ,
-	DecodeForDigit_2_0	= 0x07 ,
-	DecodeForDigit_3_0	= 0x0F ,
-	DecodeForDigit_4_0	= 0x1F ,
-	DecodeForDigit_5_0	= 0x3F ,
-	DecodeForDigit_6_0	= 0x7F ,
-	DecodeForDigit_7_0	= 0xFF ,
-}		max7219_Decode_Mode ;
 
 typedef enum {
 	ADDR_NO_OP 			= 0x00 ,
@@ -107,16 +107,18 @@ typedef enum {
 	ADDR_DISPLAY_TEST	= 0x0F
 }		Register_Address_Map	;
 
-
 /*
 **************************************************************************
 *								    DEFINES
 **************************************************************************
 */
 
-	#define PANEL_QNT				4
+	#define PANEL_QNT				2
 	#define LINE_IN_PANEL			8
-	#define	WRITE_STROB_DELAY		10
+	//#define	WRITE_STROB_DELAY		10
+	#define	WRITE_STROB_DELAY		100
+	#define BYTE_IN_SPI_PACKAGE		2
+	#define SPI_PACKAGE_TIMEOUT		20
 
 /*
 **************************************************************************
@@ -124,48 +126,16 @@ typedef enum {
 **************************************************************************
 */
 
-typedef struct
-{
+typedef struct {
 	SPI_HandleTypeDef	*spi;
 	GPIO_TypeDef 		*cs_port;
     uint16_t 			 cs_pin;
     uint8_t				data [LINE_IN_PANEL];
 	uint8_t				panel[PANEL_QNT][LINE_IN_PANEL];
 } max7219_struct;
+//----------------------------------
 
-typedef struct
-{
-//	// test - On
-//	spi_buffer[0] = 0x0F;  spi_buffer[1] = 0x01;
-//
-//	// test - Off
-//	spi_buffer[0] = 0x0F;  spi_buffer[1] = 0x00;
-//
-//	// Decode Mode - No 1 in 1
-//	// spi_buffer[1] hex   -> FF
-//	// spi_buffer[1] pixel -> 00
-//	uint8_t DecodeMode = 0xFF;
-//	spi_buffer[0] = 0x09;  spi_buffer[1] = DecodeMode;	// pixel
-//
-//
-//	// Intensity 3/32 0x01,
-//	spi_buffer[0] = 0x0A;  spi_buffer[1] = 0x03;
-//
-//
-//	//Scan Limit - All
-//	spi_buffer[0] = 0x0B;  spi_buffer[1] = 0x07;
-//
-//
-//	// Shutdown - none
-//	// spi_buffer[1] -> 00 sleep
-//	// spi_buffer[1] -> 01 work
-//	spi_buffer[0] = 0x0C;  spi_buffer[1] = 0x01;
-
-}
-max7219_init_struct;
-
-typedef enum
-{
+typedef enum {
 	most_position  = 0,
 	least_position = 1
 } position_enum;
@@ -175,21 +145,16 @@ typedef enum
 *                        GLOBAL FUNCTION PROTOTYPES
 **************************************************************************
 */
-
-	void max7219_init(max7219_struct *max7219_handler);
-	void max7219_print_value(max7219_struct *max7219_handler, uint32_t value, position_enum position);
-
 	void max7219_init2	( max7219_struct 		*max7219_handler	,
 						max7219_Decode_Mode 	_decodemode			,
 						max7219_LED_Intensity	_intensity			,
 						max7219_Scan_Limit 		_scanlimit	 		,
-						max7219_Shutdown 		_shutdown 			) ;
-//
-//	void Max7219_struct_init (	max7219_struct 		*handler		,
-//								SPI_HandleTypeDef	*_spi			,
-//								GPIO_TypeDef 		*_cs_port		,
-//								uint16_t 			 _cs_pin		) ;
+						max7219_Shutdown 		_shutdown 			);
 
+	void max7219_print_value(	max7219_struct	*max7219_handler	,
+								uint32_t		value1				,
+								uint32_t		value2				,
+								position_enum 	position			);
 
 /*
 **************************************************************************
